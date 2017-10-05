d03e264c7555ed46b309beb88da8068917717b41
Oliver Thomson Brown <otbrown91@gmail.com>
2017-10-06 00:27:53 +0100

    classes.py: rrclasses.py -> classes.py
    Class file renamed, plotting adjusted, some functions offloaded to
    helpers module

--- ridereport/classes.py
+++ ridereport/classes.py
@@ -1,4 +1,4 @@
-# rrclasses.py
+# classes.py
 # class definition file
 # Oliver Thomson Brown
 # 2017-09-29
@@ -9,6 +9,7 @@ import matplotlib.gridspec as gridspec
 import matplotlib.pyplot as plt
 import cartopy.crs as ccrs
 import cartopy.io.img_tiles as map_tiles
+from ridereport import helpers
 
 class RideReport:
     def __init__(self):
@@ -19,13 +20,16 @@ class RideReport:
         self.diff_distance = None
         self.diff_time = None
         self.distance = None
+        self.total_climb = None
         self.elapsed_time = None
         self.speed = None
         self.avg_speed = None
         self.smooth_speed = None
+        self.moving_speed = None
+        self.max_speed = None
         self.smooth_elevation = None
         self.total_time = None
-        self.total_climb = None
+        self.moving_time = None
         self.isBrevet = False
         self.brevet_distance = None
         self.brevet_speed = None
@@ -75,6 +79,8 @@ class RideReport:
         self.longitude[-1] = gps_track[-1].longitude
         self.elevation[-1] = gps_track[-1].elevation
 
+        print('{} GPS track loaded.'.format(self.name))
+
     def analyse(self):
         '''
         Calculates secondary data from the already loaded raw GPS
@@ -84,9 +90,12 @@ class RideReport:
             distance
             elapsed_time
             total_time
+            moving_time
             speed
             avg_speed
             smooth_speed
+            moving_speed
+            max_speed
             smooth_elevation
             total_climb
 
@@ -105,8 +114,11 @@ class RideReport:
         distance_window = 100
         avg_dist = self.diff_distance.mean()
         window = np.around(distance_window / avg_dist, 0).astype(int)
-        self.smooth_speed = movavg(self.speed, window)
-        self.smooth_elevation = movavg(self.elevation, window)
+        self.smooth_speed = helpers.movavg(self.speed, window)
+        self.moving_speed = self.smooth_speed.mean()
+        self.moving_time = self.distance[-1] / self.moving_speed
+        self.max_speed = self.smooth_speed.max()
+        self.smooth_elevation = helpers.movavg(self.elevation, window)
 
         diff_elevation = np.diff(self.smooth_elevation)
         self.total_climb = diff_elevation[diff_elevation > 0].sum()
@@ -124,14 +136,9 @@ class RideReport:
         self.brevet_distance = brevet_distance
         self.brevet_speed = brevet_speed
 
-        SECONDS_IN_HOUR = 60 * 60
-        METRES_IN_KM = 1000
-        time_limit = SECONDS_IN_HOUR * brevet_distance / brevet_speed
-        delta_time = time_limit - self.elapsed_time
-        delta_distance = ((self.distance[-1] - self.distance)
-                          / METRES_IN_KM)
-        self.time_in_hand = ( delta_time - (SECONDS_IN_HOUR *
-                            (delta_distance / brevet_speed)) )
+        self.time_in_hand = helpers.tih_calc(brevet_distance, brevet_speed,
+                                             self.distance,
+                                             self.elapsed_time)
 
     def delete_brevet(self):
         '''
@@ -158,9 +165,14 @@ class RideReport:
         distance_km = self.distance / METRES_IN_KM
         speed_kmh = self.smooth_speed * SECONDS_IN_HOUR / METRES_IN_KM
         avg_kmh = self.avg_speed * SECONDS_IN_HOUR / METRES_IN_KM
+        moving_kmh = self.moving_speed * SECONDS_IN_HOUR / METRES_IN_KM
+        max_kmh = self.max_speed * SECONDS_IN_HOUR / METRES_IN_KM
         hrs = np.floor(self.total_time / SECONDS_IN_HOUR)
+        moving_hrs = np.floor(self.moving_time / SECONDS_IN_HOUR)
         mins = np.around((self.total_time - (hrs * SECONDS_IN_HOUR))
                          / SECONDS_IN_MIN)
+        moving_mins = np.around((self.moving_time - (moving_hrs
+                                 * SECONDS_IN_HOUR)) / SECONDS_IN_MIN)
 
         # style and colour information
         orange = (1, 69/255, 0)
@@ -170,73 +182,57 @@ class RideReport:
         # plotting
         fig = plt.figure()
         fig.suptitle(self.name, fontsize=16, fontweight='bold')
-        if self.isBrevet:
-            gs = gridspec.GridSpec(3,2)
-        else:
-            gs = gridspec.GridSpec(2,2)
+        gs = gridspec.GridSpec(3,2)
 
         osm_tiles = map_tiles.OSM()
-        ax0 = plt.subplot(gs[:,0], projection=osm_tiles.crs)
+        ax0 = plt.subplot(gs[0:2,0], projection=osm_tiles.crs)
         ax0.plot(self.longitude, self.latitude,
                  transform=ccrs.PlateCarree(), color='red', linewidth=2)
         ax0.add_image(osm_tiles, 12)
-        notes = 'Distance: {:.2f}km\nTime: {:g}hrs {:g}mins\nClimb: {:.2f}m'.format(distance_km[-1], hrs, mins, self.total_climb)
-        ax0.text(0, -0.1, notes, transform=ax0.transAxes, fontsize=14)
 
-        ax1 = plt.subplot(gs[0,1])
-        ax1.plot(distance_km, speed_kmh, color=orange, linewidth=0.6)
-        ax1.plot(distance_km, avg_kmh, '--', linewidth=2)
+        ax1 = plt.subplot(gs[2,0])
+        row_labels = ('Distance', 'Total Climb', 'Total Time',
+                      'Moving Time', 'Moving Speed', 'Max Speed')
+        table_data = [
+                      ['{:.3f}km'.format(distance_km[-1])],
+                      ['{:.0f}m'.format(self.total_climb)],
+                      ['{:.0f} hours {:.0f} mins'.format(hrs, mins)],
+                      ['{:.0f} hours {:.0f} mins'.format(moving_hrs,
+                                                         moving_mins)],
+                      ['{:.1f}km/h'.format(moving_kmh)],
+                      ['{:.1f}km/h'.format(max_kmh)]
+                     ]
+        ax1.axis('tight')
+        ax1.axis('off')
+        ax1.table(cellText=table_data, rowLabels=row_labels,
+                  loc='center')
+
+        ax2 = plt.subplot(gs[0,1])
+        ax2.plot(distance_km, speed_kmh, color=orange, linewidth=0.6)
+        ax2.plot(distance_km, avg_kmh, '--', linewidth=2)
         if self.isBrevet:
-            ax1.plot(distance_km, (self.brevet_speed
+            ax2.plot(distance_km, (self.brevet_speed
                      * np.ones(len(self.distance))), ':k', linewidth=2)
-        ax1.set_xlabel('Distance (km)')
-        ax1.set_ylabel('Speed (km/h)')
-        if self.isBrevet:
-            ax1.legend(['Speed', 'Average Speed', 'Brevet Limit'])
-        else:
-            ax1.legend(['Speed', 'Average Speed'])
-        ax1.autoscale(tight=True)
-
-        ax2 = plt.subplot(gs[1,1])
-        ax2.fill_between(distance_km, 0, self.smooth_elevation,
-                         color=bluegrey, alpha=0.5)
         ax2.set_xlabel('Distance (km)')
-        ax2.set_ylabel('Elevation (m)')
+        ax2.set_ylabel('Speed (km/h)')
+        if self.isBrevet:
+            ax2.legend(['Speed', 'Average Speed', 'Brevet Limit'])
+        else:
+            ax2.legend(['Speed', 'Average Speed'])
         ax2.autoscale(tight=True)
 
+        ax3 = plt.subplot(gs[1,1])
+        ax3.fill_between(distance_km, 0, self.smooth_elevation,
+                         color=bluegrey, alpha=0.5)
+        ax3.set_xlabel('Distance (km)')
+        ax3.set_ylabel('Elevation (m)')
+        ax3.autoscale(tight=True)
+
         if self.isBrevet:
-            ax3 = plt.subplot(gs[2,1])
-            ax3.fill_between(distance_km, 0, self.time_in_hand)
-            ax3.set_xlabel('Distance (km)')
-            ax3.set_ylabel('Time in Hand (s)')
-            ax3.autoscale(tight=True)
+            ax4 = plt.subplot(gs[2,1])
+            ax4.fill_between(distance_km, 0, self.time_in_hand)
+            ax4.set_xlabel('Distance (km)')
+            ax4.set_ylabel('Time in Hand (s)')
+            ax4.autoscale(tight=True)
 
         plt.show()
-
-def movavg(data, window):
-    '''
-    Calculates a moving average using a particular window size.
-    The window is truncated when it meets the edge of the data.
-
-    data is a 1d numpy array, window is an integer
-    if window is odd, then it defines a region centred on the current
-    index, over which the values are averaged
-    if window is even it defines the same region as window + 1 would
-    '''
-    N = len(data)
-    window_buffer = np.floor(window / 2).astype(int)
-    smooth_data = data
-    for index in range(0, window_buffer):
-        start = 0
-        end = index + window_buffer + 1
-        smooth_data[index] = data[start:end].mean()
-    for index in range(window_buffer, N - window_buffer):
-        start = index - window_buffer
-        end = index + window_buffer + 1
-        smooth_data[index] = data[start:end].mean()
-    for index in range(N - window_buffer, N - 1):
-        start = index - window_buffer
-        end = N - 1
-        smooth_data[index] = data[start:end].mean()
-
-    return smooth_data
